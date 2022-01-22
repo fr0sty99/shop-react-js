b35683bdcb3a7a041ba750059e99d2aecd626cd2
fr0sty99 <j.neber@gmx.ch>
2022-01-22 20:23:46 +0100

    added a second style of sidebar without burger menu

--- app/src/components/HeaderBar.js
+++ app/src/components/HeaderBar.js
@@ -9,24 +9,27 @@ import './HeaderBar.css'
 
 function Sidebar() {
     const [sidebarActive, setSideBarActive] = useState(false);
+    const [useBurgerStyle, setUseBurgerStyle] = useState(true);
     const showSideBar = () => setSideBarActive(!sidebarActive);
+    const toggleBurgerStyle = () => setUseBurgerStyle(!useBurgerStyle);
     const user = "Joris Neber"
 
     return (
         <>
             <div className='header-bar'>
                 <img src={logo} className='logo' alt='Logo' />
-                <Link to='#' className='menu-bars'>
+                <Link to='#' className={useBurgerStyle ? 'menu-bars-burger' : 'menu-bars'}>
                     <FaIcons.FaBars onClick={showSideBar} />
                 </Link>
+                <a onClick={toggleBurgerStyle} style={{ cursor: 'pointer', paddingLeft: '20px', color: 'white' }}>toggle sidebar style</a>
                 <h3>{user}</h3>
                 <input type="image" className='avatar-btn' src={avatar} />
                 <button>Cart</button>
             </div>
-            <nav className={sidebarActive ? 'nav-menu active' : 'nav-menu'}>
+            <nav className={useBurgerStyle ? (sidebarActive ? 'nav-menu-burger active' : 'nav-menu-burger') : (sidebarActive ? 'nav-menu active' : 'nav-menu')}>
                 <ul className='nav-menu-items'>
-                    <li className='navbar-toggle'>
-                        <Link to='#' className='menu-bars'>
+                    <li className={useBurgerStyle ? 'navbar-toggle-burger' : 'navbar-toggle'} >
+                        <Link to='#' className={useBurgerStyle ? 'menu-bars-burger' : 'menu-bars'}>
                             <AiIcons.AiOutlineClose onClick={showSideBar} />
                         </Link>
                     </li>
